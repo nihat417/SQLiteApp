562ae510bd1a01b1b834f39dc420dd2e1a3cf117
Nihat Akremi <nihatakremi@gmail.com>
2022-11-13 14:56:40 +0300

    Добавьте файлы проекта.

---
+++ SQLiteApp/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Windows;
+namespace SQLiteApp
+{
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db = new ApplicationContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        // при загрузке окна
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // гарантируем, что база данных создана
+            db.Database.EnsureCreated();
+            // загружаем данные из БД
+            db.Users.Load();
+            // и устанавливаем данные в качестве контекста
+            DataContext = db.Users.Local.ToObservableCollection();
+        }
+
+        // добавление
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            UserWindow UserWindow = new UserWindow(new User());
+            if (UserWindow.ShowDialog() == true)
+            {
+                User User = UserWindow.User;
+                db.Users.Add(User);
+                db.SaveChanges();
+            }
+        }
+        // редактирование
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выделенный объект
+            User? user = usersList.SelectedItem as User;
+            // если ни одного объекта не выделено, выходим
+            if (user is null) return;
+
+            UserWindow UserWindow = new UserWindow(new User
+            {
+                Id = user.Id,
+                Age = user.Age,
+                Name = user.Name
+            });
+
+            if (UserWindow.ShowDialog() == true)
+            {
+                // получаем измененный объект
+                user = db.Users.Find(UserWindow.User.Id);
+                if (user != null)
+                {
+                    user.Age = UserWindow.User.Age;
+                    user.Name = UserWindow.User.Name;
+                    db.SaveChanges();
+                    usersList.Items.Refresh();
+                }
+            }
+        }
+        // удаление
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            // получаем выделенный объект
+            User? user = usersList.SelectedItem as User;
+            // если ни одного объекта не выделено, выходим
+            if (user is null) return;
+            db.Users.Remove(user);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ SQLiteApp/UserWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+namespace SQLiteApp
+{
+    public partial class UserWindow : Window
+    {
+        public User User { get; private set; }
+        public UserWindow(User user)
+        {
+            InitializeComponent();
+            User = user;
+            DataContext = User;
+        }
+
+        void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
